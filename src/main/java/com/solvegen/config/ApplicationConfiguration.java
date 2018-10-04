d0dc1dbf641cad7a239ad7ea0ac99e2f7e7139ad
Alexander Naumov <naumov.btm@gmail.com>
2018-10-04 20:21:01 +0300

    created the project. added the framework of project and needed dependencies.

---
+++ src/main/java/com/solvegen/config/ApplicationConfiguration.java
@@ -0,0 +1,7 @@
+package com.solvegen.config;
+
+/**
+ * @author Alexander Naumov
+ */
+public class ApplicationConfiguration {
+}
---
+++ src/main/java/com/solvegen/controllers/MainController.java
@@ -0,0 +1,7 @@
+package com.solvegen.controllers;
+
+/**
+ * @author Alexander Naumov
+ */
+public class MainController {
+}
